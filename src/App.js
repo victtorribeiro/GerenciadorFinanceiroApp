69991013a53feebc44529e0f153109ee9701ef09
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-03-13 17:29:52 -0300

    Terminando a tela de login
    Centralizando os componentes e capturando dados através do botão entrar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Login from './views/login';
+
 import 'bootswatch/dist/flatly/bootstrap.css'
+import './custom.css'
 
 class App extends React.Component {
   render(){
     return(
-    <div>
-      <Login />
-    </div>
-    )
+      <div>
+        <Login />
+      </div>
+      )
   };
 }
 
--- src/views/login.js
+++ src/views/login.js
@@ -1,9 +1,24 @@
+import { toHaveDescription } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
 class Login extends React.Component{
+
+    state = {
+        email: '',
+        senha: ''
+    }
+
+    
+    entrar = () => {
+        console.log('Email: ', this.state.email);
+        console.log('Senha: ', this.state.senha);
+    }
+
+
     render(){
         return(
+
             <div className="container">
                 <div className="row">
                     <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
@@ -13,14 +28,27 @@ class Login extends React.Component{
                                     <div className="col-lg-12">
                                         <div className="bs-component">
                                             <fieldset>
-
-                                                <FormGroup label='Email: *' htmlFor="exampleInputEmail1">
-                                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite o Email" />
+                                                <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                                    <input type="email" 
+                                                        value={this.state.email} 
+                                                        onChange={e => this.setState({email: e.target.value})}
+                                                        className="form-control" 
+                                                        id="exampleInputEmail1" 
+                                                        aria-describedby="emailHelp" 
+                                                        placeholder="Digite o Email" 
+                                                    />
                                                 </FormGroup>
-                                                
-                                                <FormGroup label='Senha: *' htmlFor="exampleInputPassword1">
-                                                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                                <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                    <input type="password"
+                                                        value={this.state.senha}
+                                                        onChange={e => this.setState({senha: e.target.value})}
+                                                        className="form-control"
+                                                        id="exampleInputPassword1"
+                                                        placeholder="Password" 
+                                                    />
                                                 </FormGroup>
+                                                <button onClick={ this.entrar } className="btn btn-success">Entrar</button>
+                                                <button className="btn btn-danger">Cadastrar</button>
                                             </fieldset>
                                         </div>
                                     </div>
