2be71a15ac0a34f375438eb38fbc6bcabf21a6ff
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-03-13 22:53:06 -0300

    Adicionando rotas
    Adicionando as rotas através do react-router-dom

--- src/main/App.js
+++ src/main/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import Login from './views/login';
+import Login from '../views/login';
+import CadastroUsuario from '../views/cadastroUsuario';
+import Rotas from './rotas';
 
 import 'bootswatch/dist/flatly/bootstrap.css'
-import './custom.css'
+import '../custom.css'
 
 class App extends React.Component {
   render(){
     return(
       <div>
-        <Login />
+        <Rotas />
+
       </div>
       )
   };
---
+++ src/views/cadastroUsuario.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+import Card from "../components/card";
+import FormGroup from "../components/form-group";
+
+class CadastroUsuario extends React.Component{
+
+    state = {
+        nome: '',
+        email: '',
+        senha: '',
+        senhaRepeticao: ''
+    }
+
+    cadastrar = () => {
+        console.log(this.state);
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <Card title="Cadastro de Usuário">
+
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+                                <FormGroup label="Nome: *" htmlFor="imputNome">
+                                    <input type="text"
+                                        className="form-control"
+                                        id="inputNome"
+                                        nome="nome"
+                                        onChange={e => this.setState({nome: e.target.value})}
+                                    />
+
+                                </FormGroup>
+                                <FormGroup label="Email: *" htmlFor="imputEmail">
+                                    <input type="email"
+                                        className="form-control"
+                                        id="inputEmail"
+                                        nome="email"
+                                        onChange={e => this.setState({email: e.target.value})}
+                                    />
+
+                                </FormGroup>
+                                <FormGroup label="Senha: *" htmlFor="imputSenha">
+                                    <input type="password"
+                                        className="form-control"
+                                        id="inputSenha"
+                                        nome="senha"
+                                        onChange={e => this.setState({senha: e.target.value})}
+                                    />
+
+                                </FormGroup>
+                                <FormGroup label="Repita a Senha: *" htmlFor="imputRepitaSenha">
+                                    <input type="password"
+                                        className="form-control"
+                                        id="imputRepitaSenha"
+                                        nome="senha"
+                                        onChange={e => this.setState({senhaRepeticao: e.target.value})}
+                                    />
+
+                                </FormGroup>
+                                <br></br>
+                                <button onClick={this.cadastrar} type="button" className="btn btn-success">Cadastrar</button>
+                                <button type="button" className="btn btn-danger">Cancelar</button>
+
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        );
+    }
+
+}
+
+export default CadastroUsuario;
