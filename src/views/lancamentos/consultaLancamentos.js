3bee41f03eaa1861a30957959203b30ef6c966c1
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-04-12 20:45:04 -0300

    atualizando projeto

--- src/views/lancamentos/consultaLancamentos.js
+++ src/views/lancamentos/consultaLancamentos.js
@@ -27,7 +27,6 @@ class ConsultaLancamentos extends React.Component {
         const usuarioLogado = LocalStorageService.obterItem('_usuario_logado');
         const lancamentoFiltro = {
             ano: this.state.ano,
-            
             mes: this.state.mes,
             tipo: this.state.tipo,
             usuario: usuarioLogado.id
