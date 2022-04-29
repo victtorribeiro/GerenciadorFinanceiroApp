62a9fe89819e5831d457f8bed9dda1844eb7817a
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-04-29 00:28:39 -0300

    Adicionando a biblioteca do PrimeReact

--- src/main/App.js
+++ src/main/App.js
@@ -10,6 +10,12 @@ import '../custom.css'
 import 'toastr/build/toastr.min.css'
 import 'toastr/build/toastr.css'
 
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+ 
+
 class App extends React.Component {
   render(){
     return(
