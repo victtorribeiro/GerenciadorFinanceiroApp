e3083f47723871b718daccf52c9cdad42e2dafec
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-06-04 20:58:00 -0300

    Adicionando link da aplicação no heroku

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://gerenciador-financeiro-api.herokuapp.com/'
 });
 
 class ApiService {
